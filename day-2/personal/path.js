a43c606e1d1fe0755330e3a7bf07980b1b549a04
Amirul Anwar <anwar_solong@yahoo.com.my>
2024-03-17 10:20:01 +0800

    done day 2

--- day-2/personal/path.js
+++ day-2/personal/path.js
@@ -1,5 +1,22 @@
 const path = require("path");
+const fs = require("fs");
 
-const textPath = path.join("fs-files", "7999", "messaag-7999.text");
+const textPath = path.join(__dirname, "fs-files", "7999", "message-7999.txt");
+const extFile = path.extname(textPath);
 
+// console.log(__dirname);
+// console.log(__filename);
 console.log(textPath);
+
+if (extFile === ".txt") {
+  console.log("This is a text file");
+} else {
+  console.log("This is not a text file");
+}
+
+fs.readFile(textPath, "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
