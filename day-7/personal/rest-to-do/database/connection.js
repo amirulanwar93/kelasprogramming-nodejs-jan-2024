d64d40c5847f63878821313e6cddd15f5de78c45
Amirul Anwar <anwar_solong@yahoo.com.my>
2024-04-07 07:48:44 +0800

    doe day 7

---
+++ day-7/personal/rest-to-do/database/connection.js
@@ -0,0 +1,35 @@
+import pkg from "pg";
+import createUsersTable from "../model/user.js";
+import createToDosTable from "../model/todo.js";
+const { Pool } = pkg;
+import "dotenv/config";
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export async function databaseInit() {
+  try {
+    const dbName = await pool.query("SELECT current_database()");
+    const dbRes = await pool.query("SELECT NOW()");
+    const time = dbRes.rows[0].now;
+    const name = dbName.rows[0].current_database;
+
+    console.log(`Connected to ${name} at ${time}`);
+
+    // create database tables
+    await createUsersTable();
+    await createToDosTable();
+  } catch (error) {
+    console.error(error);
+    console.error("Database connection failed");
+  }
+}
